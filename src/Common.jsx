6f1af8f6598865f5cb7f4ecf8f4ce14ea7c25c71
mkaquib <mkaquib@gmail.com>
2021-10-14 17:32:22 +0530

    new commitl

--- src/Common.jsx
+++ src/Common.jsx
@@ -12,7 +12,7 @@ export default function Common(props) {
                     <div className="col-10 mx-auto">
                     <div className="row">
                         <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
-                            <h1>Welcome to 
+                            <h1>Welcome to Aquib
                              {props.name}  <strong className="brand-name"> Jeevan</strong></h1>
                             <h2 className="my-3">
                                 We are the team of talented Doctors.
